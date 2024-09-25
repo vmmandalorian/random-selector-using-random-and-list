48743a02fbd08ee12ea71801ea56ab26a121b66b
Joshua Solidum <78305859+vmmandalorian@users.noreply.github.com>
2024-09-25 14:10:50 +0800

    Add files via upload

---
+++ PythonApplication List Python/PythonApplication_List_Python.py
@@ -0,0 +1,6 @@
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+bill_selector = random.choice(friends)
+print("The Person Who Will Pay The Bill Is")
+print(bill_selector)
